1042fbd73ab3a60f99e6a10d8a044745a272f79f
ToilaBao <thienbaodx2000@gmail.com>
2021-07-31 21:51:21 +0700

    Commit Search User's function :D

--- src/main/java/com/lmsapp/project/admin/AdminController.java
+++ src/main/java/com/lmsapp/project/admin/AdminController.java
@@ -31,10 +31,9 @@ import com.lmsapp.project.user.service.UserService;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	
 
 	private UserService userService;
-	private RoleService roleService;
-	private PasswordEncoder encoder;
 	private CourseService courseService;
 	private ModuleService moduleService;
 
@@ -42,8 +41,6 @@ public class AdminController {
 	public AdminController(UserService userService, RoleService roleService, PasswordEncoder encoder,
 			CourseService courseService, ModuleService moduleService) {
 		this.userService = userService;
-		this.roleService = roleService;
-		this.encoder = encoder;
 		this.courseService = courseService;
 		this.moduleService = moduleService;
 	}
@@ -83,13 +80,29 @@ public class AdminController {
 				"AdminController: /admin/create POST >> Created confirm password " + registration.getConfirmPassword());
 
 		try {
-			User registered = userService.registerNewUser(registration);
+			userService.registerNewUser(registration);
 		} catch (UserAlreadyExistException e) {
 			System.err.println("AdminController: /admin/create POST >> " + e.getMessage());
 			model.addAttribute("error", e.getMessage());
 		}
 
 		return url;
+	}
+	
+	@PostMapping("/search")
+	public String showListCourseBySearchCourseName(Model model, @RequestParam("searchValue") String username) {
+
+		// get course list
+		List<User> users = userService.findUsersByUsername(username);
+
+		for (User user : users) {
+			Set<Role> userRoles = user.getRoles();
+			List<String> strRoles = convertToStringList(userRoles);
+			user.setStrRoles(strRoles);
+		}
+		model.addAttribute("users", users);
+
+		return "admin/index";
 	}
 
 	//______________________________________COURSE________________________________________
